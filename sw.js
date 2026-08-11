5026651c5fe925afb865314c7cb5c222185ed897
GmeloG <goncalo.melo.goncalves@gmail.com>
2026-08-11 11:32:07 +0100

    Configurador: por ponto, mostrar/ocultar campo de foto e definir se e obrigatoria

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qc-configurador-v20";
+const CACHE = "qc-configurador-v21";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./logo.png", "./firebase-config.js"];
 const FIREBASE = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js",
