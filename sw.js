a7810b48686d7a3c6e2d23e2c34d537b48011072
GmeloG <goncalo.melo.goncalves@gmail.com>
2026-08-11 17:31:16 +0100

    Impressao: permitir tabelas quebrar entre paginas (remover espaco vazio apos Fase 1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qc-configurador-v28";
+const CACHE = "qc-configurador-v29";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./logo.png", "./firebase-config.js"];
 const FIREBASE = [
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js",
